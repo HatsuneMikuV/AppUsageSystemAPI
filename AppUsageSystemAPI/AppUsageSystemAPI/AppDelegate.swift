b9d900388c6a17ea955c056862f74196c0570af6
anglemiku <anglemiku.v@gmail.com>
2020-08-14 21:51:52 +0800

    [update] init project

---
+++ AppUsageSystemAPI/AppUsageSystemAPI/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  AppUsageSystemAPI
+//
+//  Created by AngleMiku on 2020/8/14.
+//  Copyright © 2020 AngleMiku. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+
+}
+
---
+++ AppUsageSystemAPI/AppUsageSystemAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppUsageSystemAPI
+//
+//  Created by AngleMiku on 2020/8/14.
+//  Copyright © 2020 AngleMiku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
